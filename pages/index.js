b0ce37f11af5d3a5489bc0ce7e85ee57b16d11c8
JohnOszajca <65989542+JohnOszajca@users.noreply.github.com>
2025-11-07 12:45:48 +1300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>All Nashville Roadshow – Checkout App</h1>
+      <p>This is the very first test page of your custom ticketing system.</p>
+      <p>
+        If you are seeing this online, it means GitHub and Vercel are working
+        together correctly.
+      </p>
+    </main>
+  );
+}
